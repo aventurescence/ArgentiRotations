9b07b8d006d8fa951490b35e8040d3938638a380
Churin <70804225+aventurescence@users.noreply.github.com>
2025-05-08 21:19:38 +0100

    Rename BRD369v2.cs to ChurinBRD.cs and complete rewrite of Bard Rotation. Currently in testing

--- ArgentiRotations/AssemblyInfo.cs
+++ ArgentiRotations/AssemblyInfo.cs
@@ -1,1 +1,3 @@
-﻿[assembly: AssemblyLink(Donate = "", UserName = "aventurescence", Repository = "ArgentiRotations")]
+﻿using System.Runtime.CompilerServices;
+
+[assembly: AssemblyLink(Donate = "", UserName = "aventurescence", Repository = "ArgentiRotations")]
